f28df62d27541212f1002972e2f26be2fed813e2
Alan <alangiovannyc@gmail.com>
2022-01-18 21:03:26 -0600

    cambios en js

--- public/js/modules/proyectos.js
+++ public/js/modules/proyectos.js
@@ -6,48 +6,46 @@ const btnEliminar = document.querySelector('#eliminar-proyecto');
 if (btnEliminar) {
     btnEliminar.addEventListener('click', e => {
         const urlProyecto = e.target.dataset.proyectoUrl;
+
         // console.log(urlProyecto);
         Swal.fire({
             title: 'Deseas borrar este proyecto?',
             text: "Un proyecto eliminado no se puede recuperar",
-            icon: 'warning',
+            type: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Si, Borrar!',
+            confirmButtonText: 'Si, Borrar',
             cancelButtonText: 'No, Cancelar'
         }).then((result) => {
-            if (result.isConfirmed) {
-                //enviar peticion a axios
+            if (result.value) {
+                // enviar petición a axios
                 const url = `${location.origin}/proyectos/${urlProyecto}`;
 
                 axios.delete(url, { params: { urlProyecto } })
                     .then(function (respuesta) {
+                        console.log(respuesta);
 
                         Swal.fire(
-                            'Proyecto eliminado!',
+                            'Proyecto Eliminado',
                             respuesta.data,
                             'success'
                         );
-                        //redireccionar al inicio
+
+                        // redireccionar al inicio
                         setTimeout(() => {
-                            window.location.href = "/";
-                        }, 3000)
-                    }
-                        .catch(() => {
-                            Swal.fire({
-                                type: 'error',
-                                tittle: 'Hubo un error',
-                                text: 'No se pudo eliminar el proyecto'
-                            })
+                            window.location.href = '/'
+                        }, 3000);
+                    })
+                    .catch(() => {
+                        Swal.fire({
+                            type: 'error',
+                            title: 'Hubo un error',
+                            text: 'No se pudo eliminar el Proyecto'
                         })
-
-                    );
-
-
+                    })
             }
         })
     })
 }
-
 export default btnEliminar;
